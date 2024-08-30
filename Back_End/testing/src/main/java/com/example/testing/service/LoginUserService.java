aa545855447a06780c34f0c060fb9a6f14f379e4
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-08-30 16:29:54 +0530

    remove

--- Back_End/testing/src/main/java/com/example/testing/service/LoginUserService.java
+++ Back_End/testing/src/main/java/com/example/testing/service/LoginUserService.java
@@ -5,8 +5,6 @@ import com.example.testing.dto.ReturnLoginUserDto;
 import com.example.testing.entity.LoginUser;
 import com.example.testing.repo.LoginUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
