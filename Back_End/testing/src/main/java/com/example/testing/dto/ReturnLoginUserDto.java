d22de683ee80e7a0a73084dc7da9b191df877bb5
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-08-30 14:21:23 +0530

    Add encoder and checking by email

---
+++ Back_End/testing/src/main/java/com/example/testing/dto/ReturnLoginUserDto.java
@@ -0,0 +1,14 @@
+package com.example.testing.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ReturnLoginUserDto {
+    private String email;
+    private Integer id;
+}
