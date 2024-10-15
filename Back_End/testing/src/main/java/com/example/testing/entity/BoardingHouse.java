f787bfaaf4a466e98aec15fe72867fd78b9c3085
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-10-15 14:57:11 +0530

    edit

---
+++ Back_End/testing/src/main/java/com/example/testing/entity/BoardingHouse.java
@@ -0,0 +1,13 @@
+package com.example.testing.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class BoardingHouse {
+    
+}
