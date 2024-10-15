988316e7347c35a6f172bf55be20ef43843ad7a8
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-10-15 16:52:36 +0530

    main of the home

--- Back_End/testing/src/main/java/com/example/testing/service/BoardingHouseService.java
+++ Back_End/testing/src/main/java/com/example/testing/service/BoardingHouseService.java
@@ -6,7 +6,9 @@ import com.example.testing.repo.BoardingHouseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 @Service
 public class BoardingHouseService {
@@ -20,6 +22,29 @@ public class BoardingHouseService {
         }
         BoardingHouse save = boardingHouseRepo.save(new BoardingHouse(boardingHouseDto.getCity(), boardingHouseDto.getType(), boardingHouseDto.getPhone(), boardingHouseDto.getLocation(), boardingHouseDto.getDescription(), boardingHouseDto.getEmail(), boardingHouseDto.getCity(), boardingHouseDto.getPrice(), boardingHouseDto.getStreet(), boardingHouseDto.getOwnername(), boardingHouseDto.getImage(), encodedPassword));
         return new BoardingHouseDto(save.getEmail(), save.getId());
+    }
+    public BoardingHouseDto updateBoarding(Integer id, BoardingHouseDto boardingHouseDto){
+        if(boardingHouseRepo.existsById(id)){
+            BoardingHouse update = boardingHouseRepo.save(new BoardingHouse(id,boardingHouseDto.getCity(), boardingHouseDto.getType(), boardingHouseDto.getPhone(), boardingHouseDto.getLocation(), boardingHouseDto.getDescription(), boardingHouseDto.getEmail(), boardingHouseDto.getCity(), boardingHouseDto.getPrice(), boardingHouseDto.getStreet(), boardingHouseDto.getOwnername(), boardingHouseDto.getImage(), boardingHouseDto.getPassword()));
+            return new BoardingHouseDto(boardingHouseDto.getId(), boardingHouseDto.getCity(), boardingHouseDto.getType(), boardingHouseDto.getPhone(), boardingHouseDto.getLocation(), boardingHouseDto.getDescription(), boardingHouseDto.getEmail(), boardingHouseDto.getCity(), boardingHouseDto.getPrice(), boardingHouseDto.getStreet(), boardingHouseDto.getOwnername(), boardingHouseDto.getImage(), boardingHouseDto.getPassword());
+        }
+        return null;
+    }
+    public List<BoardingHouseDto> getAllBoarding(){
+        List<BoardingHouse> all = boardingHouseRepo.findAll();
+
+        List<BoardingHouseDto> boardingHouseDtos = new ArrayList<>();
+        for(BoardingHouse boardingHouse : all){
+            boardingHouseDtos.add(new BoardingHouseDto(boardingHouse.getId(), boardingHouse.getCity(), boardingHouse.getType(), boardingHouse.getPhone(), boardingHouse.getLocation(), boardingHouse.getDescription(), boardingHouse.getEmail(), boardingHouse.getCity(), boardingHouse.getPrice(), boardingHouse.getStreet(), boardingHouse.getOwnername(), boardingHouse.getImage(), boardingHouse.getPassword()));
+        }
+        return boardingHouseDtos;
+    }
+    public int deleteBoarding(Integer id){
+        if (boardingHouseRepo.existsById(id)){
+            boardingHouseRepo.deleteById(id);
+            return 1;
+        }
+        return 0;
     }
 
 }
