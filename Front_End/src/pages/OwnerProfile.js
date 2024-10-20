2f12bdb745e1a614e2a59e776751a4ff99fe8d3a
YS Jayalal <yashodhasj6@gmail.com>
2024-10-20 20:57:00 +0530

    change Owner profile

--- Front_End/src/pages/OwnerProfile.js
+++ Front_End/src/pages/OwnerProfile.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import Preview from '../components/OwnerProfile/Preview';
 
 export default function OwnerProfile() {
   return (
-    <div className="ownerprofile">
-      <div className="ownerprofile-container">
-        <div className="preview-section">
-          <Preview/>
-        </div>
-      </div>
-     
+    <div>
+      <h1>this is Owner Profile</h1>
     </div>
   )
 }
