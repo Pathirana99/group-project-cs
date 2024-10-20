6586b9ecb926408888e46dabf4880e9b8c1c79d9
Madhushi Saranga <madhushisaranga99@gmail.com>
2024-10-20 19:42:51 +0530

    create file
    create ownerprofile folder

---
+++ Front_End/src/pages/OwnerProfile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Preview from '../components/OwnerProfile/Preview';
+
+export default function OwnerProfile() {
+  return (
+    <div className="ownerprofile">
+      <div className="ownerprofile-container">
+        <div className="preview-section">
+          <Preview/>
+        </div>
+      </div>
+     
+    </div>
+  )
+}
